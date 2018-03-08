c20bd4d24ac0d4ecb3d8110631a6d38e6b389ed2
Ron Heft <ron@thesocialstation.com>
2018-03-08 10:47:28 -0500

    Fix incorrect sensor ids

--- get-light-state.js
+++ get-light-state.js
@@ -24,3 +24,28 @@ client.groups.getAll()
       console.log();
     }
   });
+
+client.sensors.getAll()
+  .then(sensors => {
+    for (let sensor of sensors) {
+      console.log(`Sensor [${sensor.id}]: ${sensor.name}`);
+      console.log(`  Type:             ${sensor.type}`);
+      console.log(`  Manufacturer:     ${sensor.manufacturer}`);
+      console.log(`  Model Id:         ${sensor.modelId}`);
+      console.log('  Model:');
+      console.log(`    Id:             ${sensor.model.id}`);
+      console.log(`    Manufacturer:   ${sensor.model.manufacturer}`);
+      console.log(`    Name:           ${sensor.model.name}`);
+      console.log(`    Type:           ${sensor.model.type}`);
+      console.log(`  Software Version: ${sensor.softwareVersion}`);
+      console.log(`  Unique Id:        ${sensor.uniqueId}`);
+      console.log(`  Config:`);
+      console.log(`    On:             ${sensor.config.on}`);
+      console.log(`  State:`);
+      console.log(`    Last Updated:   ${sensor.state.lastUpdated}`);
+      console.log();
+    }
+  })
+  .catch(error => {
+    console.log(error.stack);
+  });
